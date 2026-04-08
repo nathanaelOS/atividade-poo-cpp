dbd14e4b998c945cafaec3edb003d2d82bb51341
nathanael de oliveira soares <nathanaeldoares2112@gmail.com>
2026-04-08 20:44:31 -0300

    main.cpp

--- atividade/main.cpp
+++ atividade/main.cpp
@@ -3,23 +3,23 @@
 #include "Usuario.hpp"
 
 int main() {
-    // Objeto Livro na STACK
+    
     Livro livroStack("Dom Casmurro", 1899, "Machado de Assis");
 
-    // Objeto Livro na HEAP (usando new)
+
     Item* livroHeap = new Livro("O Senhor dos Aneis", 1954, "J.R.R. Tolkien");
 
-    // Instanciar Usuario
+
     Usuario usuario("Natha");
 
-    // Adicionar itens ao usuario
+
     usuario.adicionarItem(&livroStack);
     usuario.adicionarItem(livroHeap);
 
-    // Listar itens usando polimorfismo
+
     usuario.listarItens();
 
-    // Liberar memoria da heap
+
     delete livroHeap;
 
     return 0;
